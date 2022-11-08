ca946df24753e42397a9fa681d834993c70171a1
Joyce Chan <joycehwchan@gmail.com>
2022-11-08 15:41:59 +0900

    add article

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :content, presence: true, length: { minimum: 10 }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+80.times do
+  Article.create(title: Faker::Books::Dune.character,
+                 content: Faker::Books::Dune.quote)
+end
